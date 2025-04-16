87d980cecf4cc9f06dcde2c6b6f8c2facbc49fbc
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 15:46:25 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jurnalModul8_103022300035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
